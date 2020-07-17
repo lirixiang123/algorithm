6f91e6c36f78dfd661f0551a99d424a21c4c405d
LIRIXIANG [李日祥] <LIRIXIANG@kingsoft.cn>
2020-07-17 15:50:11 +0800

    26

--- src/剑指offer/57- GetNext.py
+++ src/剑指offer/57- GetNext.py
@@ -8,6 +8,7 @@
 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
 """
+
 # -*- coding:utf-8 -*-
 # class TreeLinkNode:
 #     def __init__(self, x):
@@ -18,3 +19,24 @@
 class Solution:
     def GetNext(self, pNode):
         # write code here
+
+        tmp = pNode
+        while tmp.next:
+            tmp = tmp.next
+
+        self.v = []
+        self.midOrder(tmp)
+        if self.v.index(pNode) != len(self.v) - 1:
+            return self.v[self.v.index(pNode) + 1]
+        else:
+            return None
+
+
+
+    def midOrder(self,root):
+        if not root:
+            return
+
+        self.midOrder(root.left)
+        self.v.append(root)
+        self.midOrder(root.right)
---
+++ src/剑指offer/58-isSymmetrical.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/17 15:48
+# @Author  : lirixiang
+# @Email   : 565539277@qq.com
+# @File    : 58-isSymmetrical.py
+"""
+请实现一个函数，用来判断一棵二叉树是不是对称的。
+注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+"""
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def isSymmetrical(self, pRoot):
+        # write code here
+        pass
